9e4a330faac4bd75f7ab3383c01a29b0eeb776d6
Mr. Utzig <lutzig.octavius@gmail.com>
2025-05-02 16:46:52 -0300

    Done: 5/150

---
+++ algorithms/majority_element/majority_element.go
@@ -0,0 +1,32 @@
+package majority_element
+
+// Example 1:
+//
+//	Input: nums = [3,2,3]
+//	Output: 3
+var Nums1 = []int{3, 2, 3}
+
+// Example 2:
+//
+//	Input: nums = [2,2,1,1,1,2,2]
+//	Output: 2
+var Nums2 = []int{2, 2, 1, 1, 1, 2, 2}
+
+func Algorithm(nums []int) int {
+	l := len(nums)
+	h := l / 2
+	e := make(map[int]int)
+	m := 0
+
+	for i := range l {
+		v := nums[i]
+		e[v] += 1
+
+		if e[v] > h {
+			m = v
+			break
+		}
+	}
+
+	return m
+}
