468c66696c2059b277efb0a2e6c2a8bdbcbd4557
Mr. Utzig <lutzig.octavius@gmail.com>
2025-05-02 16:47:15 -0300

    Done: 7/150

---
+++ algorithms/best_time_buy_sell_stock/best_time_buy_sell_stock.go
@@ -0,0 +1,31 @@
+package best_time_buy_sell_stock
+
+// Example 1:
+//
+//	Input: prices = [7,1,5,3,6,4]
+//	Output: 5
+var Prices1 = []int{7, 1, 5, 3, 6, 4}
+
+// Example 2:
+//
+//	Input: prices = [7,6,4,3,1]
+//	Output: 0
+var Prices2 = []int{7, 6, 4, 3, 1}
+
+func Algorithm(prices []int) int {
+	cheapest := prices[0]
+	profit := 0
+	for i := 1; i < len(prices); i++ {
+		if prices[i] < cheapest {
+			cheapest = prices[i]
+		} else {
+			sell := prices[i] - cheapest
+
+			if sell > profit {
+				profit = sell
+			}
+		}
+	}
+
+	return profit
+}
