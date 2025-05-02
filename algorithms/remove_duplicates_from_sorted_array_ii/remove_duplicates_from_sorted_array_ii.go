94b0c4c846b5feec4d9029a4066f1175753adb2c
Mr. Utzig <lutzig.octavius@gmail.com>
2025-05-02 16:46:36 -0300

    Done: 4/150

---
+++ algorithms/remove_duplicates_from_sorted_array_ii/remove_duplicates_from_sorted_array_ii.go
@@ -0,0 +1,24 @@
+package remove_duplicates_from_sorted_array_ii
+
+// Example 1:
+//
+//	Input: nums = [1,1,1,2,2,3]
+//	Output: 5, nums = [1,1,2,2,3,_]
+var Nums1 = []int{1, 1, 1, 2, 2, 3}
+
+// Example 2:
+//
+//	Input: nums = [0,0,1,1,1,1,2,3,3]
+//	Output: 7, nums = [0,0,1,1,2,3,3,_,_]
+var Nums2 = []int{0, 0, 1, 1, 1, 1, 2, 3, 3}
+
+func Algorithm(nums []int) int {
+	for i := 0; i < len(nums); {
+		if len(nums) > i+2 && nums[i] == nums[i+1] && nums[i] == nums[i+2] {
+			nums = append(nums[:i], nums[i+1:]...)
+			continue
+		}
+		i++
+	}
+	return len(nums)
+}
